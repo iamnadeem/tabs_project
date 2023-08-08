013d6547e34d61d73afd7c3f269053304ef14143
Nadeem Ahmed <nadeem.a9891@gmail.com>
2023-08-08 13:56:01 +0530

    tabs project

--- src/Duties.jsx
+++ src/Duties.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import { FaAngleDoubleRight} from 'react-icons/fa';
+import {v4 as uuidv4} from 'uuid'
 
 const Duties = ({duties}) => {
     console.log(duties);
   return (
-    <div>Duties</div>
-  )
-}
+    <div>
+        {duties.map((duty, index)=> {
+            const id = uuidv4();
+            console.log(id);
 
-export default Duties
+        return (
+        <div key={index} className='job-desc'>
+            <FaAngleDoubleRight className='job-icon' />
+            <p>{duty}</p>
+        </div>
+    );
+    })}
+    </div>
+  );
+};
+
+export default Duties;
